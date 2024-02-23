6e78073ebffbb6c2e94a149b6e5bd7a8fbcd6e38
Ashishxo <dhiwarashish4@gmail.com>
2024-02-23 16:16:49 +0530

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
         }}
     >
         <div className="w-full">
-            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-xl p-5 backdrop-blur-sm bg-white/30">
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
@@ -53,7 +53,7 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-xl bg-blue-600 text-white px-2 py-0.5"
                             onClick={swap}
                         >
                             swap
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -14,7 +14,7 @@ function InputBox({
    const amountInputId = useId()
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-white p-3 rounded-2xl text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
@@ -40,7 +40,7 @@ function InputBox({
                     
                         {currencyOptions.map((currency) => (
                             <option key={currency} value={currency}>
-                            {currency}
+                            {currency.toUpperCase()}
                             </option>
                         ))}
                 
